1a1bc2706fd2fcf4053016da8c65a63767d44cbd
Tony DeVere <tdevere@microsoft.com>
2021-04-12 14:53:52 -0500

    test verification: Start event received; TrackEven received; Output logs show data from App Center

--- ThreeAmgios/ThreeAmgios/App.xaml.cs
+++ ThreeAmgios/ThreeAmgios/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using ThreeAmgios.Services;
 using ThreeAmgios.Views;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,6 +12,9 @@ namespace ThreeAmgios
 {
     public partial class App : Application
     {
+        private string ThreeAmigos_iOS = "f59a2031-9641-48c9-be98-c40db1cac3c7;";
+        private string ThreeAmigos_UWP = "15ef0072-2dd3-4910-9c17-773375e6f213";        
+        private string ThreeAmigos_Android = "549c3db4-f00b-434b-8c66-24f7c340c920";
 
         public App()
         {
@@ -19,6 +26,18 @@ namespace ThreeAmgios
 
         protected override void OnStart()
         {
+            AppCenter.LogLevel = LogLevel.Verbose;
+
+            AppCenter.Start($"ios={ThreeAmigos_iOS};android={ThreeAmigos_Android};uwp={ThreeAmigos_UWP}", typeof(Analytics), typeof(Crashes));
+
+            if (AppCenter.Configured)
+            {
+                Analytics.TrackEvent($"AppCenter Configured at {DateTime.Now.Ticks.ToString()}");
+            }
+            else
+            {
+                Analytics.TrackEvent($"AppCenter NOT Configured at {DateTime.Now.Ticks.ToString()}");
+            }
         }
 
         protected override void OnSleep()
