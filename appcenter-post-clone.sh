a53030ada1fd86b1d2958a1aea896b8f57f7181d
tdevere <tdevere@microsoft.com>
2021-11-10 15:14:18 -0600

    moved script to solution directory

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,7 @@
+﻿#/bin/bash
+
+echo "TDEVERE post clone script start - from solution directory"
+
+/bin/bash -c nuget restore 'ThreeAmgios.sln' -DisableParallelProcessing
+
+echo "TDEVERE post clone script end"
