60da72699ad30f7e19262d1cb8ea9a6cbca09b89
tdevere <tdevere@microsoft.com>
2021-11-10 15:55:48 -0600

    removed -DisableParallelProcessing

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -4,6 +4,6 @@ echo "TDEVERE post clone script start - from solution directory"
 
 ls -l
 
-/bin/bash -c nuget restore './ThreeAmgios.sln' -DisableParallelProcessing
+/bin/bash -c nuget restore './ThreeAmgios.sln' 
 
 echo "TDEVERE post clone script end"
