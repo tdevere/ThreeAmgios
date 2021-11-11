bc1e84e8d5dfd3ab120e1efa6fff996fce12cf99
tdevere <tdevere@microsoft.com>
2021-11-11 12:59:40 -0600

    dotnet restore './ThreeAmgios/ThreeAmgios.UWP/ThreeAmgios.UWP.csproj'

--- appcenter-pre-build.ps1
+++ appcenter-pre-build.ps1
@@ -3,6 +3,6 @@ Write-Host  "./ThreeAmgios/ThreeAmgios.UWP/appcenter-pre-build.sh"
 
 ls
 
-nuget restore './ThreeAmgios/ThreeAmgios.UWP/ThreeAmgios.UWP.csproj' 
+dotnet restore './ThreeAmgios/ThreeAmgios.UWP/ThreeAmgios.UWP.csproj' 
 
 Write-Host  "TDEVERE Pre-Build script end"
